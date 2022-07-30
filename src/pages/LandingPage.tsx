934df5f896a62ffc4dfbae9b2b0349ea354d6b94
Mike-Mp <mikemp@protonmail.com>
2022-07-30 16:18:28 +0300

    add image to landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,6 +4,11 @@ export default function LandingPage() {
   return (
     <Container>
       <Typography variant={"h1"}>Art Institute of Chicago</Typography>
+      <img
+        src={"https://www.lhsdoi.com/wp-content/uploads/2017/04/Art-Institute-photo.jpg"}
+        width={600}
+        height={400}
+        alt="The exterior of the Art Institute of Chicago, a classical style building with a lion statue in front." />
     </Container>
   )
 }
